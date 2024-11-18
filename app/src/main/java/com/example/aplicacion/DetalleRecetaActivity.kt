6e06c7721737dcbaa84466f36fdee79c2a51b2b1
Deyfran58 <dearagonca@est.utn.ac.cr>
2024-11-17 22:35:51 -0600

    tarea6

---
+++ app/src/main/java/com/example/aplicacion/DetalleRecetaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aplicacion
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetalleRecetaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_receta)
+    }
+}
---
+++ app/src/main/java/com/example/aplicacion/FavoritasActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aplicacion
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FavoritasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favoritas)
+    }
+}
